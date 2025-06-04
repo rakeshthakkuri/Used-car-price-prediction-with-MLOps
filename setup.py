13aadc08255673a3ae8e5a65ca887cc8270619c3
rakeshthakkuri <rakeshthakkuri969@gmail.com>
2025-06-05 02:51:33 +0530

    Introduced docker to project

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="car_price_app",
+    name="car_price_prediction_mlops",
     version="0.1",
     packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
 )
